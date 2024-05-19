15991038590c624d7f86f2a7ee8d9e0c57f84787
p1petto <ksyuha3803@mail.ru>
2024-05-19 23:07:28 +0800

    added sound, mandala

--- scripts/VBoxContainer.gd
+++ scripts/VBoxContainer.gd
@@ -13,3 +13,17 @@ func _process(delta):
 
 func _on_menu_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/root.tscn");
+
+
+func _on_menu_button_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_menu_button_3_pressed():
+	pass # Replace with function body.
+
+
+func _on_menu_button_4_pressed():
+	get_tree().quit();
+
+
---
+++ scripts/button_hover.gd
@@ -0,0 +1,27 @@
+extends AudioStreamPlayer2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_menu_button_mouse_entered():
+	play();
+
+
+func _on_menu_button_2_mouse_entered():
+	play();
+
+
+func _on_menu_button_3_mouse_entered():
+	play();
+
+
+func _on_menu_button_4_mouse_entered():
+	play();
